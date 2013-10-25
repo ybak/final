1c44d1e5d7cfd46f9ed6ceda67d8d3aa00e9ebcc
yijun.sun <dusunjun@126.com>
2013-10-25 21:19:27 +0800

    *update for test conflict

--- src/main/java/org/marathon/finalmatch/App.java
+++ src/main/java/org/marathon/finalmatch/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Something change." );
         System.out.println( "syj." );
+        System.out.println( "syj1." );
     }
 }
