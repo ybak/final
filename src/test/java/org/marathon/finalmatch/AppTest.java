2a46182d3e63d4a6990e91ebb1d82961005cef55
ybak <ybakswu@gmail.com>
2013-10-25 21:27:21 +0800

    modify test case

--- src/test/java/org/marathon/finalmatch/AppTest.java
+++ src/test/java/org/marathon/finalmatch/AppTest.java
@@ -1,34 +1,16 @@
 package org.marathon.finalmatch;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import junit.framework.Assert;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     * 
-     * @param testName
-     *            name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
+    @Test
+    public void testMethod() {
+        Assert.assertTrue(true);
     }
 }
