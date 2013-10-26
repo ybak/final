97770dc41d9c7fb7e5e37b9bd67970acae5574f8
ybak <ybakswu@gmail.com>
2013-10-26 14:47:22 +0800

    adjustment qeustion2.

---
+++ src/main/java/org/marathon/finalmatch/MaxMinAlg.java
@@ -0,0 +1,41 @@
+package org.marathon.finalmatch;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MaxMinAlg {
+
+    private Map<UserPoint, List<UserPoint>> typeDiffMap = new HashMap<UserPoint, List<UserPoint>>();
+
+    private Map<UserPoint, Double> userPointMapCache = new HashMap<UserPoint, Double>();
+
+    private UserPoint currentMaxDisPoint = null;
+
+    public Map<UserPoint, List<UserPoint>> process(List<UserPoint> allUserPoint) {
+        if (allUserPoint == null || allUserPoint.isEmpty()) {
+            return typeDiffMap;
+        }
+        UserPoint newCenter = null;
+        if (typeDiffMap.isEmpty()) {
+            List<UserPoint> typeList = new ArrayList<UserPoint>();
+            newCenter = allUserPoint.get(0);
+            typeList.add(newCenter);
+            typeDiffMap.put(newCenter, typeList);
+            allUserPoint.remove(newCenter); 
+        }
+        for (UserPoint point : allUserPoint) {
+            double distance = Math.pow(point.getX() - newCenter.getX(), 2)
+                    + Math.pow(point.getY() - newCenter.getY(), 2);
+            if (currentMaxDisPoint == null || userPointMapCache.get(currentMaxDisPoint) < distance) {
+                currentMaxDisPoint = point;
+            }
+            userPointMapCache.put(point, distance);
+        }
+        typeDiffMap.put(currentMaxDisPoint, new ArrayList<UserPoint>());
+        userPointMapCache.remove(currentMaxDisPoint);
+        return null;
+    }
+
+}
---
+++ src/main/java/org/marathon/finalmatch/UserPoint.java
@@ -0,0 +1,25 @@
+package org.marathon.finalmatch;
+
+public class UserPoint {
+
+    private Long x;//tradeTimes
+    
+    private Long y;//tradeSum
+
+    public Long getX() {
+        return x;
+    }
+
+    public void setX(Long tradeTimes) {
+        this.x = tradeTimes;
+    }
+
+    public Long getY() {
+        return y;
+    }
+
+    public void setY(Long tradeSum) {
+        this.y = tradeSum;
+    }
+    
+}
